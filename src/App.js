ddcf18d3c8bd23ccbb70911bebd8cacfe20443f3
agbomonica <agbomonica.am@gmail.com>
2022-08-08 14:56:47 +0100

    Implement page navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-// Components
+// Pages Components
 import Dashboard from "./pages/dashboard/Dashboard";
 import Signup from "./pages/signup/Signup";
 import Login from "./pages/login/Login";
 import Create from "./pages/create/Create";
 import Project from "./pages/project/Project";
+
+// Components
+import Navbar from "./components/Navbar";
 
 // Styles
 import "./App.css";
@@ -13,8 +16,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <BrowserRouter>
+      <BrowserRouter>
+        <div className="container">
+          <Navbar />
           <Routes>
             <Route index element={<Dashboard />} />
             <Route path="signup" element={<Signup />} />
@@ -23,8 +27,8 @@ function App() {
             <Route path="projects/:id" element={<Project />} />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
-        </BrowserRouter>
-      </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+
+// Styles
+import "./Navbar.css";
+import Temple from "../assets/temple.svg";
+
+function Navbar() {
+  return (
+    <header className="header">
+      <ul className="header__nav-list">
+        <li className="header__logo">
+          <img src={Temple} alt="Dojo Logo" />
+          <span>The Dogo</span>
+        </li>
+
+        <li>
+          <Link to="login">Login</Link>
+        </li>
+
+        <li>
+          <Link to="signup">Signup</Link>
+        </li>
+
+        <li>
+          <button className="btn">Logout</button>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Navbar;
